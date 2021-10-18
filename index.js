61e5c99c4f13e2894593ee1c9c86bed804172cd4
Aidan Lynch <aidantlynch00@gmail.com>
2021-10-17 22:57:03 -0400

    Added database connection

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 require('dotenv').config();
+const mysql = require('mysql');
 const discord = require('discord.js');
+
+const connection = mysql.createConnection({
+	host: process.env.DB_HOST,
+	port: process.env.DB_PORT,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_SCHEMA
+});
+connection.connect();
 
 const client = new discord.Client({
 	// intents specify the actions the bot should be able to listen to and make
